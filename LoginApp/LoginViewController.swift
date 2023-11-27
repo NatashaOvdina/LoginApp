51446a2fcdb73df1abd2785c8cc5501e7986df98
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-27 21:29:53 +0300

    Created Outlets for LoginViewController

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+
+    @IBOutlet var button: UIButton!
+    
+    @IBOutlet var forgotNameLabel: UILabel!
+    @IBOutlet var forgotPasswordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
