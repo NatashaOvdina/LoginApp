e5999cd63b129bd79ab9c635f9b1718e4a1be9f7
NatashaOvdina <natashaovdina@yahoo.com>
2023-12-02 20:39:09 +0300

    Shared values to the labels (PersonVC)

--- LoginApp/ViewControllers/ProfileViewController.swift
+++ LoginApp/ViewControllers/ProfileViewController.swift
@@ -11,10 +11,23 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet var image: UIImageView!
     
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    @IBOutlet var dateBirthLabel: UILabel!
+    @IBOutlet var educationLabel: UILabel!
+    @IBOutlet var quoteLabel: UILabel!
+    
+   private let user = User.getUser()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         makeGradientBackground()
         image.layer.cornerRadius = image.frame.width / 2
+        nameLabel.text = "Name: \(user.person.name)"
+        surnameLabel.text = "Surname: \(user.person.surname)"
+        dateBirthLabel.text = "Date of Birth: \(user.person.dateOfBirth)"
+        educationLabel.text = "Education: \(user.person.education)"
+        quoteLabel.text = "Quote: \(user.person.quote)"
     }
     
 
