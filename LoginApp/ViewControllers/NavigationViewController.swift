dc9ed27205aa39d8dd175b8891910f5c208985c1
NatashaOvdina <natashaovdina@yahoo.com>
2023-12-03 12:04:50 +0300

    Did marks in NavigationVC

--- LoginApp/ViewControllers/NavigationViewController.swift
+++ LoginApp/ViewControllers/NavigationViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 final class NavigationViewController: UINavigationController {
-    
+    // MARK: - Public Property
     var user: User!
         
+    // MARK: - Override Segue Method
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let navigationVC = segue.destination as? NavigationViewController {
             guard let profileVC = navigationVC.topViewController as? ProfileViewController else {
